c5093f3648d52d70932e4da0ee4d946352fd28a2
Manuel Aristarán <jazzido@jazzido.com>
2014-08-20 18:44:12 -0300

    comments

--- src/test/java/org/nerdpower/tabula/TestSpreadsheetExtractor.java
+++ src/test/java/org/nerdpower/tabula/TestSpreadsheetExtractor.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import java.awt.geom.Point2D;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
