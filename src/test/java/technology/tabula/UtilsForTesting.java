cbe17879ab8e7cce58b105845d832c544667adf8
Manuel Aristarán <jazzido@jazzido.com>
2016-11-22 13:17:52 -0500

    Refactor table equality assertions for better reporting

--- src/test/java/technology/tabula/UtilsForTesting.java
+++ src/test/java/technology/tabula/UtilsForTesting.java
@@ -34,19 +34,30 @@ public class UtilsForTesting {
                 oe.close();
         }
     }
-    
-    public static void assertTableEquals(Table table, String[][] arrayOfRows) {
+
+    public static String[][] tableToArrayOfRows(Table table) {
         List<List<RectangularTextContainer>> tableRows = table.getRows();
-        assertEquals(arrayOfRows.length, tableRows.size());
-        for (int i = 0; i < arrayOfRows.length; i++) {
-            String[] row = arrayOfRows[i];
-            assertEquals(row.length, tableRows.get(i).size());
-            for (int j = 0; j < row.length; j++) {
-                assertEquals(row[j].trim(), table.getCell(i, j).getText().trim());
+
+        int maxColCount = -Integer.MAX_VALUE;
+
+        for (int i = 0; i < tableRows.size(); i++) {
+            List<RectangularTextContainer> row = tableRows.get(i);
+            if (maxColCount < row.size()) {
+                maxColCount = row.size();
             }
         }
+        String[][] rv = new String[tableRows.size()][maxColCount];
+
+        for (int i = 0; i < tableRows.size(); i++) {
+            List<RectangularTextContainer> row = tableRows.get(i);
+            for (int j = 0; j < row.size(); j++) {
+                rv[i][j] = table.getCell(i, j).getText();
+            }
+        }
+
+        return rv;
     }
-    
+
     public static String loadJson(String path) throws IOException {
     	
     	BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path), "UTF-8"));
