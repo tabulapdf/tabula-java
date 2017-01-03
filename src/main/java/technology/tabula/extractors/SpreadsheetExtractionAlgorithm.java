b301ac99db64a4a352b5ca10cb156ddab6d17e41
Jeremy B. Merrill <jeremy.merrill@nytimes.com>
2017-01-03 15:42:54 -0500

    various additional null/empty checks to avoid exceptions when the user selects empty pages or regions

--- src/main/java/technology/tabula/extractors/SpreadsheetExtractionAlgorithm.java
+++ src/main/java/technology/tabula/extractors/SpreadsheetExtractionAlgorithm.java
@@ -146,6 +146,12 @@ public class SpreadsheetExtractionAlgorithm implements ExtractionAlgorithm {
     
     public boolean isTabular(Page page) {
         
+        // if there's no text at all on the page, it's not a table 
+        // (we won't be able to do anything with it though)
+        if(page.getText().isEmpty()){
+            return false; 
+        }
+
         // get minimal region of page that contains every character (in effect,
         // removes white "margins")
         Page minimalRegion = page.getArea(Utils.bounds(page.getText()));
