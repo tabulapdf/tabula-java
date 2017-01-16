19ab13101912b1524719d0cd4e24ae901cd154a7
Dean <Dean>
2017-01-16 18:14:13 -0500

    fixed java code for regex search

--- src/main/java/technology/tabula/detectors/RegexSearch.java
+++ src/main/java/technology/tabula/detectors/RegexSearch.java
@@ -15,12 +15,9 @@ public class RegexSearch {
 	// these should go somewhere else
 	// private globals?
 	// reset methods?
-	int size;
 	char currChar;
-	String textString;
 
 	Boolean upperExists;
-	Boolean lowerExists;
 	int upperCount;
 	int lowerCount;
 
@@ -34,155 +31,111 @@ public class RegexSearch {
 
 	public List<Rectangle> detect(Page page, String upperBound, String lowerBound) throws ParseException {
 		List<Rectangle> regexList = new ArrayList<Rectangle>();
-		try {
-			size = 0;
-			textString = "";
+		currChar = 0;
+		upperExists = false;
+		upperCount = 0;
+		lowerCount = 0;
+		currElement = null;
+		firstElement = null;
+		lastElement = null;
+		state = 0;
 
-			currChar = 0;
-			upperExists = false;
-			lowerExists = false;
-			upperCount = 0;
-			lowerCount = 0;
-			currElement = null;
-			firstElement = null;
-			lastElement = null;
-			state = 0;
+		// have page, can check text elements
+		textElements = page.getText();
+		int size = textElements.size();
 
-			// have page, can check text elements
-
-			textElements = page.getText();
-			size = textElements.size();
-
-			// this is only good for quick testing
-			// should scan through text elements so that we can use coords
-			for (int i = 0; i < size; i++) {
-				currElement = textElements.get(i);
-				currChar = currElement.getText().charAt(0);
-				// textString = textString.concat(currChar);
-
+		// should scan through text elements so that we can use coords
+		for (int i = 0; i < size; i++) {
+			currElement = textElements.get(i);
+			currChar = currElement.getText().charAt(0);
+			if (!upperBound.isEmpty() && upperExists == false) {
 				switch (state) {
-				// no matches, look for first char of upper string
-				case 0: {
-					// valid first char detected
-					if (currChar == upperBound.charAt(0)) {
+				case 0:
+					if (currChar == upperBound.charAt(0)) { // valid first upper
 						firstElement = currElement;
-
-						// guards against single character case
-						if (upperBound.length() == 1) {
-							upperExists = true;
-							state = 2;
-						}
-						// multiple character strings continue scanning
-						else {
+						if (upperBound.length() == 1) { // single character case
+							state = 0;
+							upperCount = 0;
+							if (lowerBound.isEmpty()) {
+								// single character upper, no lower
+								regexList.add(
+										new Rectangle(firstElement.y, 0, page.width, page.height - firstElement.y));
+							} else {
+								// single character upper, yes lower
+								upperExists = true;
+							}
+						} else { // multiple character strings continue scanning
 							upperCount = 1;
 							state = 1;
 						}
 					}
 					break;
-				}
-				// first char of upper string detected, look for the rest of
-				// the upper string chars
-				case 1: {
-					// next valid char detected
+				case 1:
 					if (currChar == upperBound.charAt(upperCount)) {
 						upperCount++;
-					}
-					// invalid char detected
-					else {
+					} else { // invalid char detected
 						upperCount = 0;
 						state = 0;
 					}
-
 					if (upperCount == upperBound.length()) {
-						upperExists = true;
-						state = 2;
+						state = 0;
+						upperCount = 0;
+						if (lowerBound.isEmpty()) {
+							// Multiple character upper, no lower
+							regexList.add(new Rectangle(firstElement.y, 0, page.width, page.height - firstElement.y));
+						} else {
+							// Multiple character upper, yes lower
+							upperExists = true;
+						}
 					}
-
 					break;
 				}
-				// upper string matched, look for first char of lower string
-				case 2: {
-					// valid first char detected
-					if (currChar == lowerBound.charAt(0)) {
-						// guards against single character case
+			} else {
+				switch (state) {
+				case 0:
+					if (currChar == lowerBound.charAt(0)) { // valid first lower
 						if (lowerBound.length() == 1) {
 							lastElement = currElement;
-							lowerExists = true;
-
-							state = 4;
+							state = 0;
+							if (upperExists) { // upper and lower single char
+								regexList.add(
+										new Rectangle(firstElement.y, 0, page.width, lastElement.y - firstElement.y));
+							} else {
+								regexList.add(new Rectangle(0, 0, page.width, lastElement.y));
+							}
+							upperExists = false;
 							continue;
-						}
-						// multiple character strings continue scanning
-						else {
+						} else {// multiple character strings continue scanning
 							lowerCount = 1;
-							state = 3;
+							state = 1;
 						}
 					}
-
 					break;
-				}
-				// first char of lower string detected, look for rest of
-				// lower string chars
-				case 3: {
+				case 1:
 					// next valid char detected
 					if (currChar == lowerBound.charAt(lowerCount)) {
 						lowerCount++;
-					}
-					// invalid char detected
-					else {
+					} else { // invalid char detected
 						lowerCount = 0;
-						state = 2;
+						state = 0;
 					}
 					if (lowerCount == lowerBound.length()) {
 						lastElement = currElement;
-						lowerExists = true;
-
-						state = 4;
-
-						continue;
+						state = 0;
+						lowerCount = 0;
+						if (upperExists) {
+							regexList.add(new Rectangle(firstElement.y, 0, page.width, lastElement.y - firstElement.y));
+						} else {
+							regexList.add(new Rectangle(0, 0, page.width, lastElement.y));
+						}
+						upperExists = false;
 					}
 					break;
 				}
-				// lower string matched, output coordinates and exit?
-				case 4: {
-					regexList.add(new Rectangle(firstElement.x, firstElement.y, lastElement.x - firstElement.x,
-							lastElement.y - firstElement.y));
-					System.out.println("Upper Coordinates: \n  x = " + firstElement.x + "\n  y = " + firstElement.y);
-					System.out
-							.println("Lower Coordinates: \n  x = " + lastElement.x + "\n  y = " + lastElement.y + "\n");
-
-					// may want to keep scanning the page? reset locals here
-					// and go to state 0?
-					// store coordinates somewhere?
-
-					// dumb way to reset locals
-					currChar = 0;
-					upperExists = false;
-					lowerExists = false;
-					upperCount = 0;
-					lowerCount = 0;
-					currElement = null;
-					firstElement = null;
-					lastElement = null;
-					state = 0;
-
-					totalMatches++;
-
-					// i = size; // jump out of this page, locals
-					// automatically get reset
-					// state = 4;
-				}
-				// you shouldn't be here
-				default: {
-					state = 0;
-					break;
-				}
-				}
 			}
-			System.out.println("Total Matches = " + totalMatches);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.err.println("Error: " + e.getMessage());
+		}
+		for (Rectangle walk : regexList) {
+			System.out.println("x=" + walk.x + "\ny=" + walk.y + "\nwidth=" + walk.width + "\nheight=" + walk.height);
 		}
 		return regexList;
 	}
