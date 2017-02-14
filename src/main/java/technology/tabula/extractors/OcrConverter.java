9bea9c8be61f302bd2b4b26bb0a64a50914c2944
Dean <Dean>
2017-02-14 17:33:02 -0500

    OCR Full conversion complete, dumped into individual pdf's folder

--- src/main/java/technology/tabula/extractors/OcrConverter.java
+++ src/main/java/technology/tabula/extractors/OcrConverter.java
@@ -5,13 +5,38 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 // Tess4j imports
-import net.sourceforge.tess4j.*;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import net.sourceforge.tess4j.util.LoadLibs;
 import net.sourceforge.tess4j.util.PdfUtilities;
 
 public class OcrConverter {
 	public String extract(String input_filepath) {
-		
-		
-		return "conversion complete.";
+		ArrayList<ITesseract.RenderedFormat> list = new ArrayList<ITesseract.RenderedFormat>();
+		list.add(ITesseract.RenderedFormat.PDF);
+
+		// prepare input PDF - specify page subset
+		// PdfUtilities.splitPdf("WellExample.pdf", "parsedInput.pdf", "1",
+		// "1");
+		File image;
+		try {
+			// extract images from PDF
+			image = PdfUtilities.convertPdf2Tiff(new File(input_filepath));
+			ITesseract instance = new Tesseract();
+			instance.setDatapath(LoadLibs.extractTessResources("tessdata").getParent());
+
+			// output - create PDF from image above
+			String image_filepath = image.getAbsolutePath();
+			instance.createDocuments(image_filepath, input_filepath.substring(0, input_filepath.length() - 4) + "_OCR",
+					list);
+			System.out.println("OCR Done");
+			return input_filepath.substring(0, input_filepath.length() - 4) + "_OCR.pdf";
+		} catch (TesseractException e) {
+			System.err.println(e.getMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "Failed";
 	}
 }
