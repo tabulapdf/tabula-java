fffae84544a35ea0e76163b68139c4b68c9c8ce6
Daniel Gross <dlgross@ncsu.edu>
2017-02-14 16:27:59 -0500

    added Tess4j dependency to pom.xml, added base class for ocr implementation

---
+++ src/main/java/technology/tabula/extractors/OcrConverter.java
@@ -0,0 +1,17 @@
+package technology.tabula.extractors;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+// Tess4j imports
+import net.sourceforge.tess4j.*;
+import net.sourceforge.tess4j.util.PdfUtilities;
+
+public class OcrConverter {
+	public String extract(String input_filepath) {
+		
+		
+		return "conversion complete.";
+	}
+}
