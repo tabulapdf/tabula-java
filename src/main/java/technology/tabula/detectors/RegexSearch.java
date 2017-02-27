c721649b119b9999221276151d12cddce58714ff
mtricha3 <mtricha3@ncsu.edu>
2017-02-26 19:34:16 -0500

    Add files via upload
    files for alpha demo

--- src/main/java/technology/tabula/detectors/RegexSearch.java
+++ src/main/java/technology/tabula/detectors/RegexSearch.java
@@ -10,40 +10,24 @@ import technology.tabula.Rectangle;
 import technology.tabula.TextElement;
 
 public class RegexSearch {
-
-	private List<TextElement> textElements;
-	// these should go somewhere else
-	// private globals?
-	// reset methods?
-	char currChar;
-
-	Boolean upperExists;
-	int upperCount;
-	int lowerCount;
-
-	TextElement currElement;
-	TextElement firstElement;
-	TextElement lastElement;
-
-	int totalMatches = 0;
-
-	int state = 0;
-
-	public List<Rectangle> detect(Page page, String upperLeft, String upperRight, String lowerLeft, String lowerRight)
-			throws ParseException {
+	
+	// test without expanding to page?
+	public List<Rectangle> detect(Page page, String upperBound, String lowerBound) throws ParseException {
 		List<Rectangle> regexList = new ArrayList<Rectangle>();
-		currChar = 0;
-		upperExists = false;
-		upperCount = 0;
-		lowerCount = 0;
-		currElement = null;
-		firstElement = null;
-		lastElement = null;
-		state = 0;
-		String upperBound = upperLeft;
-		String lowerBound = lowerLeft;
+		char currChar = 0;
+		Boolean upperExists = false;
+		int upperCount = 0;
+		int lowerCount = 0;
+		TextElement currElement = null;
+		TextElement firstElement = null;
+		TextElement lastElement = null;
+		int state = 0;
+
+		
+		// check for empty upper and lower here instead of later
+		
 		// have page, can check text elements
-		textElements = page.getText();
+		List<TextElement> textElements = page.getText();
 		int size = textElements.size();
 
 		// should scan through text elements so that we can use coords
@@ -136,6 +120,295 @@ public class RegexSearch {
 				}
 			}
 		}
+		for (Rectangle walk : regexList) {
+			System.out.println("x=" + walk.x + "\ny=" + walk.y + "\nwidth=" + walk.width + "\nheight=" + walk.height);
+		}
+		return regexList;
+	}
+	
+	// possible issues:
+	//					multiple strings share the same name
+	//					right string to the left of left string?
+	//						need first char of left strings, last char of right strings <-- if left and right reversed???
+	//					recover loop value if string detect fails
+	// todo:
+	//					implement empty field handling
+	public List<Rectangle> detect(Page page, String upperLeft, String upperRight, String lowerLeft, String lowerRight) throws ParseException {
+		
+		// if both top strings are null, return null
+		if(upperLeft == null && upperRight == null)
+		{
+			return new ArrayList<Rectangle>(); // empty list
+		}
+		
+		else if(lowerLeft == null && lowerRight == null)
+		{
+			return new ArrayList<Rectangle>(); // empty list
+		}
+		
+		List<Rectangle> regexList = new ArrayList<Rectangle>();
+		char currChar = 0;
+		
+		// could check elements for null instead
+		Boolean upperLeftExists = false;
+		Boolean upperRightExists = false;
+		Boolean lowerLeftExists = false;
+		Boolean lowerRightExists = false;
+		
+		TextElement upperLeftElement = null;
+		TextElement upperRightElement = null;
+		TextElement lowerLeftElement = null;
+		TextElement lowerRightElement = null;
+		
+		int backup = 0;
+		
+		int upperLeftCount = 0;
+		int upperRightCount = 0;
+		int lowerLeftCount = 0;
+		int lowerRightCount = 0;
+		
+		TextElement currElement = null;
+		TextElement firstElement = null;
+		//TextElement lastElement = null;
+		
+		int upperState = 0;
+		int lowerState = 0;
+				
+		// have page, can check text elements
+		List<TextElement> textElements = page.getText();
+		int size = textElements.size();
+
+		// should scan through text elements so that we can use coords
+		for (int i = 0; i < size; i++) {
+			
+			currElement = textElements.get(i);
+			currChar = currElement.getText().charAt(0);
+			
+			// search for top elements
+			if(!(upperLeftExists && upperRightExists))
+			{
+				switch(upperState)
+				{
+					case 0: // search for first char of either top string
+					{
+						if (currChar == upperLeft.charAt(0) && !(upperLeftExists)) // valid first upper left
+						{
+							if (upperLeft.length() == 1) // single character case
+							{
+								upperLeftElement = currElement;
+								upperLeftExists = true;
+							}
+							
+							else
+							{
+								firstElement = currElement;
+								backup = i;
+								upperLeftCount = 1;
+								upperState = 1;
+							}
+						}
+						
+						else if(currChar == upperRight.charAt(0) && !(upperRightExists)) // valid first upper right
+						{
+							if (upperRight.length() == 1) // single character case
+							{
+								upperRightElement = currElement;
+								upperRightExists = true;
+							}
+							else
+							{
+								firstElement = currElement;
+								backup = i;
+								upperRightCount = 1;
+								upperState = 2;
+							}
+						}
+						
+						break;
+					}
+				
+					case 1: // evaluate chars for top left string
+					{
+						if (currChar == upperLeft.charAt(upperLeftCount)) 
+						{
+							upperLeftCount++;
+						}
+						else 
+						{ // invalid char detected
+							upperLeftCount = 0;
+							upperState = 0;
+							i = backup+1;
+						}
+						
+						if (upperLeftCount == upperLeft.length()) 
+						{
+							upperLeftElement = firstElement;
+							upperLeftExists = true;
+							firstElement = null;
+							
+							if(upperRightExists)
+								upperState = 3;
+							else
+								upperState = 0;												
+						}
+						
+						break;
+					}
+					
+					case 2: // evaluate chars for top right string
+					{
+						if (currChar == upperRight.charAt(upperRightCount)) 
+						{
+							upperRightCount++;
+						}
+						else 
+						{ // invalid char detected
+							upperRightCount = 0;
+							upperState = 0;
+							i = backup+1;
+						}
+						
+						if (upperRightCount == upperRight.length()) 
+						{
+							upperRightElement = currElement;
+							upperRightExists = true;
+							firstElement = null;
+							
+							if(upperLeftExists)
+								upperState = 3;
+							else
+								upperState = 0;
+						}
+						
+						break;
+					}
+					
+					case 3: // found both upper strings, probably not needed because of if statement
+					{
+						// zzz
+					}
+				}
+			}
+			
+			else
+			{
+				switch(lowerState)
+				{
+					case 0: // search for first char of either top string
+					{
+						if (currChar == lowerLeft.charAt(0) && !(lowerLeftExists)) // valid first lower left
+						{
+							if (lowerLeft.length() == 1) // single character case
+							{
+								lowerLeftElement = currElement;
+								lowerLeftExists = true;
+							}
+							
+							else
+							{
+								firstElement = currElement;
+								backup = i;
+								lowerLeftCount = 1;
+								lowerState = 1;
+							}
+						}
+						
+						else if(currChar == lowerRight.charAt(0) && !(lowerRightExists)) // valid first lower right
+						{
+							if (lowerRight.length() == 1) // single character case
+							{
+								lowerRightElement = currElement;
+								lowerRightExists = true;
+							}
+							else
+							{
+								firstElement = currElement;
+								backup = i;
+								lowerRightCount = 1;
+								lowerState = 2;
+							}
+						}
+						
+						break;
+					}
+				
+					case 1: // evaluate chars for top left string
+					{
+						if (currChar == lowerLeft.charAt(lowerLeftCount)) 
+						{
+							lowerLeftCount++;
+						}
+						else 
+						{ // invalid char detected
+							lowerLeftCount = 0;
+							lowerState = 0;
+							i = backup+1;
+						}
+						
+						if (lowerLeftCount == lowerLeft.length()) 
+						{
+							lowerLeftElement = firstElement;
+							lowerLeftExists = true;
+							firstElement = null;
+							
+							if(lowerRightExists)
+								lowerState = 3;
+							else
+								lowerState = 0;												
+						}
+						
+						break;
+					}
+					
+					case 2: // evaluate chars for top right string
+					{
+						if (currChar == lowerRight.charAt(lowerRightCount)) 
+						{
+							lowerRightCount++;
+						}
+						else 
+						{ // invalid char detected
+							lowerRightCount = 0;
+							lowerState = 0;
+							i = backup+1;
+						}
+						
+						if (lowerRightCount == lowerRight.length()) 
+						{
+							lowerRightElement = currElement;
+							lowerRightExists = true;
+							firstElement = null;
+							
+							if(lowerLeftExists)
+								lowerState = 3;
+							else
+								lowerState = 0;
+						}
+						
+						break;
+					}
+					
+					case 3: // found both lower strings, probably not needed because of if statement
+					{
+						// zzz
+					}
+				}
+			}			
+		} // end of for loop
+		
+		if(!(upperLeftExists && upperRightExists && lowerLeftExists && lowerRightExists)) // missing item, placeholder
+		{
+			return new ArrayList<Rectangle>(); // empty arraylist
+		}
+		
+		float leftBound = Math.min(upperLeftElement.x, lowerLeftElement.x);
+		float topBound = Math.min(upperLeftElement.y, upperRightElement.y);
+		float width = Math.max((upperRightElement.x + upperRightElement.width - leftBound), (lowerRightElement.x + lowerRightElement.width - leftBound));
+		float height = Math.max((lowerLeftElement.y + lowerLeftElement.height - topBound),(lowerRightElement.y + lowerRightElement.height - topBound));
+		
+		Rectangle foundRectangle = new Rectangle(topBound, leftBound, width, height); 
+		regexList.add(foundRectangle);
+		
 		for (Rectangle walk : regexList) {
 			System.out.println("x=" + walk.x + "\ny=" + walk.y + "\nwidth=" + walk.width + "\nheight=" + walk.height);
 		}
