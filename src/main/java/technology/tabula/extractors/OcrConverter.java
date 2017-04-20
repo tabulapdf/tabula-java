46daf5cfc8e06e044d84b826e2f3f5c99400276d
Daniel Gross <dlgross@ncsu.edu>
2017-04-20 18:07:23 -0400

    removed backend cause of need for renaming operations.

--- src/main/java/technology/tabula/extractors/OcrConverter.java
+++ src/main/java/technology/tabula/extractors/OcrConverter.java
@@ -2,6 +2,8 @@ package technology.tabula.extractors;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
 import javax.imageio.spi.IIORegistry;
@@ -27,6 +29,15 @@ public class OcrConverter {
 		// "1");
 		File image;
 		try {
+			// rename document.pdf -> document_image.pdf
+			//Path source = new File(input_filepath).toPath();
+			//Files.move(source, source.resolveSibling("document_image.pdf"));
+			
+			//String document_imagePath = input_filepath.substring(0, input_filepath.length() - 4) + "_image.pdf";
+			
+			//File document_imageFile = new File(document_imagePath);
+			//new File(input_filepath).renameTo(document_imageFile);	// document.pdf -> document_image.pdf
+			
 			// extract images from PDF
 			image = PdfUtilities.convertPdf2Tiff(new File(input_filepath));
 			ITesseract instance = new Tesseract();
@@ -34,14 +45,21 @@ public class OcrConverter {
 
 			// output - create PDF from image above
 			String image_filepath = image.getAbsolutePath();
-			instance.createDocuments(image_filepath, input_filepath.substring(0, input_filepath.length() - 4) + "_OCR",
-					list);
+			instance.createDocuments(image_filepath, input_filepath.substring(0, input_filepath.length() - 4), list);
 			System.out.println("OCR Done");
+			
+//			new File(input_filepath.substring(0, input_filepath.length() - 4) + "_OCR.pdf").renameTo(new File(input_filepath));	// document_OCR.pdf -> document.pdf
 			return "Success";
 		} catch (TesseractException e) {
 			System.err.println(e.getMessage());
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			System.out.println(input_filepath);
+			System.out.println(input_filepath.substring(0, input_filepath.length() - 4) + "_image.pdf");
 		}
 		return "Failed";
 	}
