c2591890873f9b628dfeb2fd877ad0f61d02ad7d
mattrich37 <mattrich37@gmail.com>
2017-05-09 14:30:15 -0400

    basic 1 string search, should have fixed bounding box issues

--- src/main/java/technology/tabula/detectors/RegexSearch.java
+++ src/main/java/technology/tabula/detectors/RegexSearch.java
@@ -63,7 +63,7 @@ public class RegexSearch {
 		}
 		
 		// if 2 fields are null, determine which then perform 2 corner search
-		if(emptyCount == 2 
+		else if(emptyCount == 2 
 				&& (upperLeft.equals("") ^ upperRight.equals("")) 
 				&& (lowerLeft.equals("") ^ lowerRight.equals(""))) {
 			String upperBound;
@@ -75,15 +75,123 @@ public class RegexSearch {
 			return detectTwo(page, upperBound, lowerBound);
 		}
 		
+		// if 3 fields are null, determine which then perform single string search
+		else if(emptyCount == 3){
+			// determine which field is not empty
+			String searchString;
+			if(!upperLeft.equals("")) searchString = upperLeft;
+			else if(!upperRight.equals("")) searchString = upperRight;
+			else if(!lowerLeft.equals("")) searchString = lowerLeft;
+			else if(!lowerRight.equals("")) searchString = lowerRight;
+			else return new ArrayList<Rectangle>(); // return empty arraylist
+			
+			return detectString(page, searchString);
+		}
+		
 		return new ArrayList<Rectangle>(); // return empty arraylist
 		
 	}
 	
-	// test without expanding to page?
+	private List<Rectangle> detectString(Page page, String searchString) {		
+		List<Rectangle> regexList = new ArrayList<Rectangle>();
+		char currChar = 0;
+		TextElement currElement = null;
+		TextElement firstElement = null;
+		TextElement lastElement = null;
+		int state = 0;
+		
+		int stringCount = 0;
+		boolean stringFound = false;
+
+		// check for empty upper and lower here instead of later
+		// Dont really need to check these if they are private and only called by detect function				
+		if(page == null){
+			return new ArrayList<Rectangle>(); // empty list
+		}
+		if(searchString == null){
+			return new ArrayList<Rectangle>(); // empty list
+		}
+		
+		// have page, can check text elements
+		List<TextElement> textElements = page.getText();
+		int size = textElements.size();
+
+		// should scan through text elements so that we can use coords
+		for (int i = 0; i < size; i++) {
+			currElement = textElements.get(i);
+			currChar = currElement.getText().charAt(0);
+			
+			if(!stringFound){
+				switch(state){
+				case 0:
+					if(currChar == searchString.charAt(0)) { // valid first upper
+						firstElement = currElement;
+						
+						// check for single character case
+						if(searchString.length() == 1){
+							stringFound = true;
+							regexList.add(new Rectangle(firstElement.y,
+														firstElement.x,
+														firstElement.width,
+														firstElement.height));
+						}
+						// start searching for next characters in string
+						else{
+							stringCount = 1;
+							state = 1;
+						}					
+					}
+					break;
+					
+				case 1:
+					if (currChar == searchString.charAt(stringCount)) {
+						stringCount++;
+					} 
+					else { // invalid char detected
+						stringCount = 0;
+						state = 0;
+					}
+					if (stringCount == searchString.length()) {
+						lastElement = currElement;
+						stringFound = true;
+						
+						stringCount = 0;
+						state = 0;
+					}
+					break;				
+				}
+			}
+			else break;
+		}
+		
+		if(!stringFound) return new ArrayList<Rectangle>(); // empty list
+		else{			
+			// assume that first and last elements could appear on different lines
+			// this method will not draw a proper box encapsulating all elements of string
+			//		if elements are on different lines
+			float topBound = Math.min(firstElement.y, lastElement.y);
+			float botBound = Math.max(firstElement.y + firstElement.height, lastElement.y + lastElement.height);
+			float leftBound = Math.min(firstElement.x - firstElement.width, lastElement.x - lastElement.width);
+			float rightBound = Math.max(firstElement.x + firstElement.width, lastElement.x + firstElement.width);
+			float width = rightBound - leftBound;
+			float height = botBound - topBound;
+			
+			Rectangle stringBound = new Rectangle(topBound, leftBound, width, height);			
+			
+			// try to find overlapping autodetect table for box surrounding string
+			// Todo
+			
+			regexList.add(stringBound);
+		}
+		
+		return regexList;
+	}
+
 	private List<Rectangle> detectTwo(Page page, String upperBound, String lowerBound) throws ParseException {
 		List<Rectangle> regexList = new ArrayList<Rectangle>();
 		char currChar = 0;
 		Boolean upperExists = false;
+		Boolean lowerExists = false;
 		int upperCount = 0;
 		int lowerCount = 0;
 		TextElement currElement = null;
@@ -96,7 +204,8 @@ public class RegexSearch {
 		if(page == null){
 			return new ArrayList<Rectangle>(); // empty list
 		}
-		if(upperBound == null || lowerBound == null){
+		// check for null or empty fields
+		if(upperBound == null || lowerBound == null || upperBound.isEmpty() || lowerBound.isEmpty()){
 			return new ArrayList<Rectangle>(); // empty list
 		}
 
@@ -104,11 +213,11 @@ public class RegexSearch {
 		List<TextElement> textElements = page.getText();
 		int size = textElements.size();
 
-		// should scan through text elements so that we can use coords
+		// scan through list of text elements for character arrays that match input strings
 		for (int i = 0; i < size; i++) {
 			currElement = textElements.get(i);
 			currChar = currElement.getText().charAt(0);
-			if (!upperBound.isEmpty() && upperExists == false) {
+			if (!upperExists) {
 				switch (state) {
 				case 0:
 					if (currChar == upperBound.charAt(0)) { // valid first upper
@@ -116,14 +225,7 @@ public class RegexSearch {
 						if (upperBound.length() == 1) { // single character case
 							state = 0;
 							upperCount = 0;
-							if (lowerBound.isEmpty()) {
-								// single character upper, no lower
-								regexList.add(new Rectangle(firstElement.y - firstElement.height, 0, page.width,
-										page.height - firstElement.y));
-							} else {
-								// single character upper, yes lower
-								upperExists = true;
-							}
+							upperExists = true;						
 						} else { // multiple character strings continue scanning
 							upperCount = 1;
 							state = 1;
@@ -140,32 +242,19 @@ public class RegexSearch {
 					if (upperCount == upperBound.length()) {
 						state = 0;
 						upperCount = 0;
-						if (lowerBound.isEmpty()) {
-							// Multiple character upper, no lower
-							regexList.add(new Rectangle(firstElement.y - firstElement.height, 0, page.width,
-									page.height - firstElement.y));
-						} else {
-							// Multiple character upper, yes lower
-							upperExists = true;
-						}
+						upperExists = true;						
 					}
 					break;
 				}
-			} else {
+			} else if(!lowerExists) {
 				switch (state) {
 				case 0:
 					if (currChar == lowerBound.charAt(0)) { // valid first lower
 						if (lowerBound.length() == 1) {
 							lastElement = currElement;
 							state = 0;
-							if (upperExists) { // upper and lower single char
-								regexList.add(new Rectangle(firstElement.y - firstElement.height, 0, page.width,
-										(lastElement.y - firstElement.y) + lastElement.height));
-							} else {
-								regexList.add(new Rectangle(0, 0, page.width, lastElement.y + lastElement.height));
-							}
-							upperExists = false;
-							continue;
+							lowerCount = 0;
+							lowerExists = true;
 						} else {// multiple character strings continue scanning
 							lowerCount = 1;
 							state = 1;
@@ -184,22 +273,26 @@ public class RegexSearch {
 						lastElement = currElement;
 						state = 0;
 						lowerCount = 0;
-						if (upperExists) {
-							regexList.add(new Rectangle(firstElement.y - firstElement.height, 0, page.width,
-									(lastElement.y - firstElement.y) + lastElement.height));
-						} else {
-							regexList.add(new Rectangle(0, 0, page.width, lastElement.y + lastElement.height));
-						}
-						upperExists = false;
+						lowerExists = true;
 					}
 					break;
 				}
-			}
+			} else break;
 		}
-		/*for (Rectangle walk : regexList) {
-			System.out.println(
-					"Box Found at x=" + walk.x + " y=" + walk.y + " width=" + walk.width + " height=" + walk.height);
-		}*/
+		
+		if(upperExists && lowerExists){
+			float topBound = Math.min(firstElement.y, lastElement.y);
+			float botBound = Math.max(firstElement.y + firstElement.height, lastElement.y + lastElement.height);
+			float leftBound = 0; // expand found table to page
+			float rightBound = page.width; // expand found table to page
+			float width = rightBound - leftBound;
+			float height = botBound - topBound;
+			
+			Rectangle foundRectangle = new Rectangle(topBound, leftBound, width, height);
+			
+			regexList.add(foundRectangle);
+		}
+		
 		return regexList;
 	}
 
@@ -211,8 +304,6 @@ public class RegexSearch {
 	// recover loop value if string detect fails
 	// two strings on same line with same first char may fail to correctly reset
 	// todo:
-	// implement empty field handling
-	//
 	// check for both strings at once in first state, then set flag
 	private List<Rectangle> detectFour(Page page, String upperLeft, String upperRight, String lowerLeft, String lowerRight)
 			throws ParseException {
@@ -532,31 +623,54 @@ public class RegexSearch {
 		}
 
 		// this is sooooo dumb
-		float leftBound;
 		float topBound;
-		float width;
-		float height;
+		float botBound;
+		float leftBound;
+		float rightBound;
+		
+		// should this not be y?
+		if(!upperLeftExists && upperRightExists) topBound = upperRightElement.y - upperRightElement.height;
+		else if(upperLeftExists && !upperRightExists) topBound = upperLeftElement.y - upperLeftElement.height;
+		else topBound = Math.min(upperLeftElement.y - upperLeftElement.height,
+				upperRightElement.y - upperRightElement.height);
+		
+		/*
+		if(!upperLeftExists && upperRightExists) topBound = upperRightElement.x - upperRightElement.width;
+		else if(upperLeftExists && !upperRightExists) topBound = upperLeftElement.x - upperLeftElement.width;
+		else topBound = Math.min(upperLeftElement.y - upperLeftElement.height,
+				upperRightElement.y - upperRightElement.height);
+		 */
+		
+		if(!lowerRightExists && lowerLeftExists) botBound = lowerLeftElement.y + lowerLeftElement.height;
+		else if(lowerRightExists && !lowerLeftExists) botBound = lowerRightElement.y + lowerRightElement.height;
+		else botBound = Math.max((lowerLeftElement.y + lowerLeftElement.height),
+				(lowerRightElement.y + lowerRightElement.height));
 		
 		if(!upperLeftExists && lowerLeftExists) leftBound = lowerLeftElement.x - lowerLeftElement.width;
 		else if(upperLeftExists && !lowerLeftExists) leftBound = upperLeftElement.x - upperLeftElement.width;
 		else leftBound = Math.min(upperLeftElement.x - upperLeftElement.width,
 				lowerLeftElement.x - lowerLeftElement.width);
 		
-		if(!upperLeftExists && upperRightExists) topBound = upperRightElement.x - upperRightElement.width;
-		else if(upperLeftExists && !upperRightExists) topBound = upperLeftElement.x - upperLeftElement.width;
-		else topBound = Math.min(upperLeftElement.y - upperLeftElement.height,
-				upperRightElement.y - upperRightElement.height);
+		if(!upperRightExists && lowerRightExists) rightBound = lowerRightElement.x + 2*lowerRightElement.width;
+		else if(upperRightExists && !lowerRightExists) rightBound = upperRightElement.x + 2*upperRightElement.width;
+		else rightBound = Math.max((upperRightElement.x + 2*upperRightElement.width),
+				(lowerRightElement.x + 2*lowerRightElement.width));
 		
+		/*
 		if(!upperRightExists && lowerRightExists) width = lowerRightElement.x + lowerRightElement.width - leftBound;
 		else if(upperRightExists && !lowerRightExists) width = upperRightElement.x + upperRightElement.width - leftBound;
 		else width = Math.max((upperRightElement.x + upperRightElement.width - leftBound),
 				(lowerRightElement.x + lowerRightElement.width - leftBound));
 		
+		
 		if(!lowerRightExists && lowerLeftExists) height = lowerLeftElement.y + lowerLeftElement.height - topBound;
 		else if(lowerRightExists && !lowerLeftExists) height = lowerRightElement.y + lowerRightElement.height - topBound;
 		else height = Math.max((lowerLeftElement.y + lowerLeftElement.height - topBound),
 				(lowerRightElement.y + lowerRightElement.height - topBound));
+		*/
 		
+		float width = rightBound - leftBound;
+		float height = botBound - topBound;
 		
 		/*
 		// may want to check if parameters match left/right schema first
