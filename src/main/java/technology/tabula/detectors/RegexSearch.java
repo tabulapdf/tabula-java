9b5259fe83888b11907c2df5a13122cd30993435
mtricha3 <mtricha3@ncsu.edu>
2017-05-09 20:48:52 -0400

    one string search now compares to autodetect

--- src/main/java/technology/tabula/detectors/RegexSearch.java
+++ src/main/java/technology/tabula/detectors/RegexSearch.java
@@ -165,7 +165,7 @@ public class RegexSearch {
 		}
 		
 		if(!stringFound) return new ArrayList<Rectangle>(); // empty list
-		else{			
+		else{
 			// assume that first and last elements could appear on different lines
 			// this method will not draw a proper box encapsulating all elements of string
 			//		if elements are on different lines
@@ -176,12 +176,51 @@ public class RegexSearch {
 			float width = rightBound - leftBound;
 			float height = botBound - topBound;
 			
-			Rectangle stringBound = new Rectangle(topBound, leftBound, width, height);			
+			Rectangle eval = new Rectangle(topBound, leftBound, width, height);			
 			
-			// try to find overlapping autodetect table for box surrounding string
-			// Todo
+			// try to find overlapping autodetect table for box surrounding string			
+			// get auto detected list of rectangles
+			NurminenDetectionAlgorithm nurmDetect = new NurminenDetectionAlgorithm();
+			List<Rectangle> auto = nurmDetect.detect(page);
+
+			// try spreadsheet in case auto doesnt work
+			SpreadsheetDetectionAlgorithm spreadDetect = new SpreadsheetDetectionAlgorithm();
+			List<Rectangle> spread = spreadDetect.detect(page);
+
+			float bestOverlap = 0;
+			Rectangle bestGuess = eval;
 			
-			regexList.add(stringBound);
+			// compare with autodetected tables
+			if (!auto.isEmpty()) {
+				for (int k = 0; k < auto.size(); k++) {
+					Rectangle eval1 = auto.get(k);
+
+					// look for rectangle overlap
+					float overlap = eval.overlapRatio(eval1);
+
+					if (overlap > bestOverlap) {
+						bestOverlap = overlap;
+						bestGuess = eval1;
+					}
+				}
+			}
+
+			// compare with spreadsheet tables
+			if (!spread.isEmpty()) {
+				for (int k = 0; k < spread.size(); k++) {
+					Rectangle eval2 = spread.get(k);
+
+					// look for rectangle overlap
+					float overlap = eval.overlapRatio(eval2);
+
+					if (overlap > bestOverlap) {
+						bestOverlap = overlap;
+						bestGuess = eval2;
+					}
+				}
+			}
+			
+			regexList.add(bestGuess);
 		}
 		
 		return regexList;
@@ -199,7 +238,6 @@ public class RegexSearch {
 		TextElement lastElement = null;
 		int state = 0;
 
-		// check for empty upper and lower here instead of later
 		// Dont really need to check these if they are private and only called by detect function				
 		if(page == null){
 			return new ArrayList<Rectangle>(); // empty list
@@ -296,15 +334,6 @@ public class RegexSearch {
 		return regexList;
 	}
 
-	// possible issues:
-	// multiple strings share the same name
-	// right string to the left of left string?
-	// need first char of left strings, last char of right strings <-- if left
-	// and right reversed???
-	// recover loop value if string detect fails
-	// two strings on same line with same first char may fail to correctly reset
-	// todo:
-	// check for both strings at once in first state, then set flag
 	private List<Rectangle> detectFour(Page page, String upperLeft, String upperRight, String lowerLeft, String lowerRight)
 			throws ParseException {
 
@@ -348,7 +377,6 @@ public class RegexSearch {
 
 		TextElement currElement = null;
 		TextElement firstElement = null;
-		// TextElement lastElement = null;
 
 		int searchArea = 0;
 		int upperState = 0;
@@ -368,246 +396,246 @@ public class RegexSearch {
 				searchArea = 1;
 			}
 			
+			// this part of loop body is very difficult to modify and should be revamped
 			try{
-			switch(searchArea) {
-			case 0:
-			// search for top elements
-			//if (!(upperLeftExists && upperRightExists)) {
-				switch (upperState) {
-				case 0: // search for first char of either top string
-				{
-					// valid first upper left
-					if (upperLeft.startsWith(Character.toString(currChar)) && !(upperLeftExists))
+				switch(searchArea) {
+				case 0:
+				// search for top elements
+				//if (!(upperLeftExists && upperRightExists)) {
+					switch (upperState) {
+					case 0: // search for first char of either top string
 					{
-						if (upperLeft.length() == 1) // single character case
+						// valid first upper left
+						if (upperLeft.startsWith(Character.toString(currChar)) && !(upperLeftExists))
 						{
-							upperLeftElement = currElement;
+							if (upperLeft.length() == 1) // single character case
+							{
+								upperLeftElement = currElement;
+								upperLeftExists = true;
+								lastGoodIndex = i;
+								foundCount++;
+							}
+	
+							else {
+								firstElement = currElement;
+								backup = i;
+								upperLeftCount = 1;
+								upperState = 1;
+							}
+						}
+	
+						// valid first upper right
+						else if (upperRight.startsWith(Character.toString(currChar)) && !(upperRightExists))
+						{
+							if (upperRight.length() == 1) // single character case
+							{
+								upperRightElement = currElement;
+								upperRightExists = true;
+								lastGoodIndex = i;
+								foundCount++;
+							} else {
+								firstElement = currElement;
+								backup = i;
+								upperRightCount = 1;
+								upperState = 2;
+							}
+						}
+	
+						break;
+					}
+	
+					case 1: // evaluate chars for top left string
+					{
+						if (currChar == upperLeft.charAt(upperLeftCount)) {
+							upperLeftCount++;
+						} else { // invalid char detected
+							upperLeftCount = 0;
+	
+							//if (upperLeft.charAt(0) == upperRight.charAt(0)) {
+							if (upperRight.startsWith(Character.toString(upperLeft.charAt(0)))) {
+								upperRightCount = 1;
+								upperState = 2;
+								i = backup; // go back to first character
+							} else {
+								upperState = 0;
+								i = backup; // go to second character
+							}
+						}
+	
+						if (upperLeftCount == upperLeft.length()) {
+							upperLeftElement = firstElement;
 							upperLeftExists = true;
+							firstElement = null;
+							
 							lastGoodIndex = i;
 							foundCount++;
+	
+							if (upperRightExists)
+								upperState = 3;
+							else
+								upperState = 0;
 						}
-
-						else {
-							firstElement = currElement;
-							backup = i;
-							upperLeftCount = 1;
-							upperState = 1;
-						}
+	
+						break;
 					}
-
-					// valid first upper right
-					else if (upperRight.startsWith(Character.toString(currChar)) && !(upperRightExists))
+	
+					case 2: // evaluate chars for top right string
 					{
-						if (upperRight.length() == 1) // single character case
-						{
+						if (currChar == upperRight.charAt(upperRightCount)) {
+							upperRightCount++;
+						} else { // invalid char detected
+							upperRightCount = 0;
+							upperState = 0;
+							i = backup;
+						}
+	
+						if (upperRightCount == upperRight.length()) {
 							upperRightElement = currElement;
 							upperRightExists = true;
+							firstElement = null;
+							
 							lastGoodIndex = i;
 							foundCount++;
-						} else {
-							firstElement = currElement;
-							backup = i;
-							upperRightCount = 1;
-							upperState = 2;
+	
+							if (upperLeftExists)
+								upperState = 3; // replace
+							else
+								upperState = 0;
 						}
+	
+						break;
 					}
-
-					break;
+	
+					case 3: // found both upper strings, probably not needed because
+							// of if statement
+					{
+						break;
+					}
 				}
-
-				case 1: // evaluate chars for top left string
-				{
-					if (currChar == upperLeft.charAt(upperLeftCount)) {
-						upperLeftCount++;
-					} else { // invalid char detected
-						upperLeftCount = 0;
-
-						//if (upperLeft.charAt(0) == upperRight.charAt(0)) {
-						if (upperRight.startsWith(Character.toString(upperLeft.charAt(0)))) {
-							upperRightCount = 1;
-							upperState = 2;
-							i = backup; // go back to first character
-						} else {
-							upperState = 0;
-							i = backup; // go to second character
-						}
-					}
-
-					if (upperLeftCount == upperLeft.length()) {
-						upperLeftElement = firstElement;
-						upperLeftExists = true;
-						firstElement = null;
-						
-						lastGoodIndex = i;
-						foundCount++;
-
-						if (upperRightExists)
-							upperState = 3;
-						else
-							upperState = 0;
-					}
-
+					
 					break;
-				}
-
-				case 2: // evaluate chars for top right string
-				{
-					if (currChar == upperRight.charAt(upperRightCount)) {
-						upperRightCount++;
-					} else { // invalid char detected
-						upperRightCount = 0;
-						upperState = 0;
-						i = backup;
-					}
-
-					if (upperRightCount == upperRight.length()) {
-						upperRightElement = currElement;
-						upperRightExists = true;
-						firstElement = null;
-						
-						lastGoodIndex = i;
-						foundCount++;
-
-						if (upperLeftExists)
-							upperState = 3; // replace
-						else
-							upperState = 0;
-					}
-
-					break;
-				}
-
-				case 3: // found both upper strings, probably not needed because
-						// of if statement
-				{
-					break;
-				}
-			}
 				
-				break;
-			
-			case 1: // lower strings
-			//else {
-				switch (lowerState) {
-				case 0: // search for first char of either top string
-				{
-					// valid first lower left
-					if (lowerLeft.startsWith(Character.toString(currChar)) && !(lowerLeftExists))
+				case 1: // lower strings
+				//else {
+					switch (lowerState) {
+					case 0: // search for first char of either top string
 					{
-						if (lowerLeft.length() == 1) // single character case
+						// valid first lower left
+						if (lowerLeft.startsWith(Character.toString(currChar)) && !(lowerLeftExists))
 						{
-							lowerLeftElement = currElement;
+							if (lowerLeft.length() == 1) // single character case
+							{
+								lowerLeftElement = currElement;
+								lowerLeftExists = true;
+								lastGoodIndex = i;
+								foundCount++;
+							}
+	
+							else {
+								firstElement = currElement;
+								backup = i;
+								lowerLeftCount = 1;
+								lowerState = 1;
+							}
+						}
+	
+						// valid first lower right
+						else if (lowerRight.startsWith(Character.toString(currChar)) && !(lowerRightExists))
+						{
+							if (lowerRight.length() == 1) // single character case
+							{
+								lowerRightElement = currElement;
+								lowerRightExists = true;
+								lastGoodIndex = i;
+								foundCount++;
+							} else {
+								firstElement = currElement;
+								backup = i;
+								lowerRightCount = 1;
+								lowerState = 2;
+							}
+						}
+	
+						break;
+					}
+	
+					case 1: // evaluate chars for top left string
+					{
+						if (currChar == lowerLeft.charAt(lowerLeftCount)) {
+							lowerLeftCount++;
+						} else { // invalid char detected
+							lowerLeftCount = 0;
+	
+							//if (lowerLeft.charAt(0) == lowerRight.charAt(0)) {
+							if (lowerRight.startsWith(Character.toString(lowerLeft.charAt(0)))) {
+								lowerRightCount = 1;
+								lowerState = 2;
+								i = backup; // go back to first character
+							} else {
+								lowerState = 0;
+								i = backup; // go to second character
+							}
+						}
+	
+						if (lowerLeftCount == lowerLeft.length()) {
+							lowerLeftElement = firstElement;
 							lowerLeftExists = true;
+							firstElement = null;
+							
 							lastGoodIndex = i;
 							foundCount++;
+	
+							if (lowerRightExists)
+								lowerState = 3;
+							else
+								lowerState = 0;
 						}
-
-						else {
-							firstElement = currElement;
-							backup = i;
-							lowerLeftCount = 1;
-							lowerState = 1;
-						}
+	
+						break;
 					}
-
-					// valid first lower right
-					else if (lowerRight.startsWith(Character.toString(currChar)) && !(lowerRightExists))
+	
+					case 2: // evaluate chars for top right string
 					{
-						if (lowerRight.length() == 1) // single character case
-						{
+						if (currChar == lowerRight.charAt(lowerRightCount)) {
+							lowerRightCount++;
+						} else { // invalid char detected
+							lowerRightCount = 0;
+							lowerState = 0;
+							i = backup;
+						}
+	
+						if (lowerRightCount == lowerRight.length()) {
 							lowerRightElement = currElement;
 							lowerRightExists = true;
+							firstElement = null;
+							
 							lastGoodIndex = i;
 							foundCount++;
-						} else {
-							firstElement = currElement;
-							backup = i;
-							lowerRightCount = 1;
-							lowerState = 2;
+	
+							if (lowerLeftExists)
+								lowerState = 3;
+							else
+								lowerState = 0;
 						}
+	
+						break;
 					}
-
+	
+					case 3: 
+					{
+						// zzz
+						break;
+					}
+				}
+					
 					break;
 				}
-
-				case 1: // evaluate chars for top left string
-				{
-					if (currChar == lowerLeft.charAt(lowerLeftCount)) {
-						lowerLeftCount++;
-					} else { // invalid char detected
-						lowerLeftCount = 0;
-
-						//if (lowerLeft.charAt(0) == lowerRight.charAt(0)) {
-						if (lowerRight.startsWith(Character.toString(lowerLeft.charAt(0)))) {
-							lowerRightCount = 1;
-							lowerState = 2;
-							i = backup; // go back to first character
-						} else {
-							lowerState = 0;
-							i = backup; // go to second character
-						}
-					}
-
-					if (lowerLeftCount == lowerLeft.length()) {
-						lowerLeftElement = firstElement;
-						lowerLeftExists = true;
-						firstElement = null;
-						
-						lastGoodIndex = i;
-						foundCount++;
-
-						if (lowerRightExists)
-							lowerState = 3;
-						else
-							lowerState = 0;
-					}
-
-					break;
-				}
-
-				case 2: // evaluate chars for top right string
-				{
-					if (currChar == lowerRight.charAt(lowerRightCount)) {
-						lowerRightCount++;
-					} else { // invalid char detected
-						lowerRightCount = 0;
-						lowerState = 0;
-						i = backup;
-					}
-
-					if (lowerRightCount == lowerRight.length()) {
-						lowerRightElement = currElement;
-						lowerRightExists = true;
-						firstElement = null;
-						
-						lastGoodIndex = i;
-						foundCount++;
-
-						if (lowerLeftExists)
-							lowerState = 3;
-						else
-							lowerState = 0;
-					}
-
-					break;
-				}
-
-				case 3: 
-				{
-					// zzz
-					break;
-				}
-			}
-				
-				break;
-			}
 			}
 			// this is so dumb
 			catch(NullPointerException npe){
 				//System.out.println(npe);
 			}
 			
-			// DONT LET THIS BECOME AN INF LOOP!
 			// if at end of page and only one upper string detected, need to roll back to last good index
 			// then begin searching for the bottom strings
 			if((i == (size - 1)) && !(upperRightExists && upperLeftExists) && (foundCount == 1) && (resetCount == 0)){
@@ -622,7 +650,6 @@ public class RegexSearch {
 			return new ArrayList<Rectangle>(); // empty arraylist
 		}
 
-		// this is sooooo dumb
 		float topBound;
 		float botBound;
 		float leftBound;
@@ -633,13 +660,6 @@ public class RegexSearch {
 		else if(upperLeftExists && !upperRightExists) topBound = upperLeftElement.y - upperLeftElement.height;
 		else topBound = Math.min(upperLeftElement.y - upperLeftElement.height,
 				upperRightElement.y - upperRightElement.height);
-		
-		/*
-		if(!upperLeftExists && upperRightExists) topBound = upperRightElement.x - upperRightElement.width;
-		else if(upperLeftExists && !upperRightExists) topBound = upperLeftElement.x - upperLeftElement.width;
-		else topBound = Math.min(upperLeftElement.y - upperLeftElement.height,
-				upperRightElement.y - upperRightElement.height);
-		 */
 		
 		if(!lowerRightExists && lowerLeftExists) botBound = lowerLeftElement.y + lowerLeftElement.height;
 		else if(lowerRightExists && !lowerLeftExists) botBound = lowerRightElement.y + lowerRightElement.height;
@@ -656,41 +676,12 @@ public class RegexSearch {
 		else rightBound = Math.max((upperRightElement.x + 2*upperRightElement.width),
 				(lowerRightElement.x + 2*lowerRightElement.width));
 		
-		/*
-		if(!upperRightExists && lowerRightExists) width = lowerRightElement.x + lowerRightElement.width - leftBound;
-		else if(upperRightExists && !lowerRightExists) width = upperRightElement.x + upperRightElement.width - leftBound;
-		else width = Math.max((upperRightElement.x + upperRightElement.width - leftBound),
-				(lowerRightElement.x + lowerRightElement.width - leftBound));
-		
-		
-		if(!lowerRightExists && lowerLeftExists) height = lowerLeftElement.y + lowerLeftElement.height - topBound;
-		else if(lowerRightExists && !lowerLeftExists) height = lowerRightElement.y + lowerRightElement.height - topBound;
-		else height = Math.max((lowerLeftElement.y + lowerLeftElement.height - topBound),
-				(lowerRightElement.y + lowerRightElement.height - topBound));
-		*/
-		
 		float width = rightBound - leftBound;
 		float height = botBound - topBound;
-		
-		/*
-		// may want to check if parameters match left/right schema first
-		float leftBound = Math.min(upperLeftElement.x - upperLeftElement.width,
-				lowerLeftElement.x - lowerLeftElement.width);
-		float topBound = Math.min(upperLeftElement.y - upperLeftElement.height,
-				upperRightElement.y - upperRightElement.height);
-		float width = Math.max((upperRightElement.x + upperRightElement.width - leftBound),
-				(lowerRightElement.x + lowerRightElement.width - leftBound));
-		float height = Math.max((lowerLeftElement.y + lowerLeftElement.height - topBound),
-				(lowerRightElement.y + lowerRightElement.height - topBound));
-		*/
 		
 		Rectangle foundRectangle = new Rectangle(topBound, leftBound, width, height);
 		regexList.add(foundRectangle);
 
-		/*for (Rectangle walk : regexList) {
-			System.out.println(
-					"Box found at x=" + walk.x + " y=" + walk.y + " width=" + walk.width + " height=" + walk.height);
-		}*/
 		return regexList;
 	}
 }
