eb671dfd67d3871864537b089f4aa88b815038bc
Alexander Belov <fourlex@users.noreply.github.com>
2017-07-25 16:26:47 +0300

    Delete duplicated addOption

--- src/main/java/technology/tabula/CommandLineApp.java
+++ src/main/java/technology/tabula/CommandLineApp.java
@@ -273,7 +273,6 @@ public class CommandLineApp {
         o.addOption("v", "version", false, "Print version and exit.");
         o.addOption("h", "help", false, "Print this help text.");
         o.addOption("g", "guess", false, "Guess the portion of the page to analyze per page.");
-        o.addOption("d", "debug", false, "Print detected table areas instead of processing");
         o.addOption("r", "spreadsheet", false, "[Deprecated in favor of -l/--lattice] Force PDF to be extracted using spreadsheet-style extraction (if there are ruling lines separating each cell, as in a PDF of an Excel spreadsheet)");
         o.addOption("n", "no-spreadsheet", false, "[Deprecated in favor of -t/--stream] Force PDF not to be extracted using spreadsheet-style extraction (if there are no ruling lines separating each cell)");
         o.addOption("l", "lattice", false, "Force PDF to be extracted using lattice-mode extraction (if there are ruling lines separating each cell, as in a PDF of an Excel spreadsheet)");
