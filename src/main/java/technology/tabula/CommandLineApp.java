ebc83ac2bb1a1cbe54ab8081d70f3c9fe81886ea
Manuel Aristarán <jazzido@jazzido.com>
2020-09-03 10:30:58 -0300

    Prepare for next release

--- src/main/java/technology/tabula/CommandLineApp.java
+++ src/main/java/technology/tabula/CommandLineApp.java
@@ -29,7 +29,7 @@ import technology.tabula.writers.Writer;
 
 public class CommandLineApp {
 
-    private static String VERSION = "1.0.4";
+    private static String VERSION = "1.0.5";
     private static String VERSION_STRING = String.format("tabula %s (c) 2012-2020 Manuel Aristarán", VERSION);
     private static String BANNER = "\nTabula helps you extract tables from PDFs\n\n";
 
