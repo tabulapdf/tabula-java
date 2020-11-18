eadb4e1f56d808b22f453650ea1e83cb4f25aba0
Zaqueu Cavalcante <zaqueudovale@gmail.com>
2020-11-18 11:13:43 -0300

    Refactoring writers.

--- src/main/java/technology/tabula/writers/JSONWriter.java
+++ src/main/java/technology/tabula/writers/JSONWriter.java
@@ -1,9 +1,5 @@
 package technology.tabula.writers;
 
-import java.io.IOException;
-import java.lang.reflect.Modifier;
-import java.util.List;
-
 import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
@@ -17,33 +13,49 @@ import technology.tabula.TextChunk;
 import technology.tabula.json.RectangularTextContainerSerializer;
 import technology.tabula.json.TableSerializer;
 
+import java.io.IOException;
+import java.util.List;
+
+import static java.lang.reflect.Modifier.PUBLIC;
+
 public class JSONWriter implements Writer {
 
-	private static final ExclusionStrategy ALLCLASSES_SKIPNONPUBLIC = new ExclusionStrategy() {
-		@Override public boolean shouldSkipClass(Class<?> c) { return false; }
-		@Override public boolean shouldSkipField(FieldAttributes fa) { return !fa.hasModifier(Modifier.PUBLIC); }
-	};
+    private static final ExclusionStrategy ALL_CLASSES_SKIPPING_NON_PUBLIC_FIELDS = new ExclusionStrategy() {
+        @Override
+        public boolean shouldSkipClass(Class<?> c) {
+            return false;
+        }
 
-	@Override
-	public void write(Appendable out, Table table) throws IOException {
-		out.append(gson().toJson(table, Table.class));
-	}
+        @Override
+        public boolean shouldSkipField(FieldAttributes fieldAttributes) {
+            return !fieldAttributes.hasModifier(PUBLIC);
+        }
+    };
 
-	@Override public void write(Appendable out, List<Table> tables) throws IOException {
-		Gson gson = gson();
-		JsonArray array = new JsonArray();
-		for (Table table : tables) array.add(gson.toJsonTree(table, Table.class));
-		out.append(gson.toJson(array));
-	}
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+    @Override
+    public void write(Appendable out, Table table) throws IOException {
+        out.append(gson().toJson(table, Table.class));
+    }
 
-	private static Gson gson() {
-		return new GsonBuilder()
-				.addSerializationExclusionStrategy(ALLCLASSES_SKIPNONPUBLIC)
-				.registerTypeAdapter(Table.class, TableSerializer.INSTANCE)
-				.registerTypeAdapter(RectangularTextContainer.class, RectangularTextContainerSerializer.INSTANCE)
-				.registerTypeAdapter(Cell.class, RectangularTextContainerSerializer.INSTANCE)
-				.registerTypeAdapter(TextChunk.class, RectangularTextContainerSerializer.INSTANCE)
-				.create();
-	}
+    @Override
+    public void write(Appendable out, List<Table> tables) throws IOException {
+        Gson gson = gson();
+        JsonArray jsonElements = new JsonArray();
+        for (Table table : tables)
+            jsonElements.add(gson.toJsonTree(table, Table.class));
+        out.append(gson.toJson(jsonElements));
+    }
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+    private static Gson gson() {
+        return new GsonBuilder()
+                .addSerializationExclusionStrategy(ALL_CLASSES_SKIPPING_NON_PUBLIC_FIELDS)
+                .registerTypeAdapter(Table.class, TableSerializer.INSTANCE)
+                .registerTypeAdapter(RectangularTextContainer.class, RectangularTextContainerSerializer.INSTANCE)
+                .registerTypeAdapter(Cell.class, RectangularTextContainerSerializer.INSTANCE)
+                .registerTypeAdapter(TextChunk.class, RectangularTextContainerSerializer.INSTANCE)
+                .create();
+    }
 
 }
--- src/main/java/technology/tabula/writers/Writer.java
+++ src/main/java/technology/tabula/writers/Writer.java
@@ -6,6 +6,9 @@ import java.util.List;
 import technology.tabula.Table;
 
 public interface Writer {
+
     void write(Appendable out, Table table) throws IOException;
+
     void write(Appendable out, List<Table> tables) throws IOException;
+
 }
