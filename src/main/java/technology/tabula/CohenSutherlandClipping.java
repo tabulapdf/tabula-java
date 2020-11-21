b2f8753e3e19545310f913237a8ec83787e2ae56
Zaqueu Cavalcante <zaqueudovale@gmail.com>
2020-11-21 10:13:08 -0300

    Encapsulating point attributes.

--- src/main/java/technology/tabula/CohenSutherlandClipping.java
+++ src/main/java/technology/tabula/CohenSutherlandClipping.java
@@ -34,100 +34,108 @@ public final class CohenSutherlandClipping {
     private final static float MINIMUM_DELTA = 0.01f;
 
     /**
-     * Creates a Cohen Sutherland clipper with clip rect (0, 0, 0, 0).
+     * Creates a Cohen Sutherland clipper with clip window (0, 0, 0, 0).
      */
     public CohenSutherlandClipping() {}
 
     /**
-     * Creates a Cohen Sutherland clipper with the given clip rectangle.
-     * @param clip the clip rectangle to use
+     * Creates a Cohen Sutherland clipper with the given clip window.
+     * @param clipWindow the clip window to use.
      */
-    public CohenSutherlandClipping(Rectangle2D clip) {
-        setClip(clip);
+    public CohenSutherlandClipping(Rectangle2D clipWindow) {
+        setClip(clipWindow);
     }
 
     /**
      * Sets the clip rectangle.
-     * @param clip the clip rectangle
+     * @param clipWindow the clip window.
      */
-    public void setClip(Rectangle2D clip) {
-        xMin = clip.getX();
-        xMax = xMin + clip.getWidth();
-        yMin = clip.getY();
-        yMax = yMin + clip.getHeight();
+    public void setClip(Rectangle2D clipWindow) {
+        xMin = clipWindow.getX();
+        xMax = xMin + clipWindow.getWidth();
+        yMin = clipWindow.getY();
+        yMax = yMin + clipWindow.getHeight();
     }
 
-    private final int regionCode(double x, double y) {
-        int code = (x < xMin) ? LEFT : (x > xMax) ? RIGHT : INSIDE;
-        if (y < yMin)
-            code |= BOTTOM;
-        else if (y > yMax)
-            code |= TOP;
-        return code;
-    }
-
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
     /**
-     * Clips a given line against the clip rectangle.
+     * Clips a given line against the clip window.
      * The modification (if needed) is done in place.
      * @param line the line to clip.
      * @return true if line is clipped, false if line is
-     * totally outside the clip rect.
+     * totally outside the clip window.
      */
     public boolean clip(Line2D.Float line) {
-        double point1X = line.getX1(), point1Y = line.getY1();
-        double point2X = line.getX2(), point2Y = line.getY2();
-        double outsidePointX = 0d, outsidePointY = 0d;
+        Point point1 = new Point(line.getX1(), line.getY1());
+        Point point2 = new Point(line.getX2(), line.getY2());
+        Point outsidePoint = new Point(0d, 0d);
 
-        boolean lineIsVertical = (point1X == point2X);
-        double lineSlope = lineIsVertical ? 0d : (point2Y-point1Y)/(point2X-point1X);
+        boolean lineIsVertical = (point1.x == point2.x);
+        double lineSlope = lineIsVertical ? 0d : (point2.y-point1.y)/(point2.x-point1.x);
 
-        int point1Region = regionCode(point1X, point1Y);
-        int point2Region = regionCode(point2X, point2Y);
+        while (point1.region != INSIDE || point2.region != INSIDE) {
+            if ((point1.region & point2.region) != INSIDE) return false;
 
-        while (point1Region != INSIDE || point2Region != INSIDE) {
-            if ((point1Region & point2Region) != INSIDE)
-                return false;
+            outsidePoint.region = (point1.region == INSIDE) ? point2.region : point1.region;
 
-            int outsidePointRegion = (point1Region == INSIDE) ? point2Region : point1Region;
-
-            if ((outsidePointRegion & LEFT) != INSIDE) {
-                outsidePointX = xMin;
-                outsidePointY = delta(outsidePointX, point1X)*lineSlope + point1Y;
+            if ((outsidePoint.region & LEFT) != INSIDE) {
+                outsidePoint.x = xMin;
+                outsidePoint.y = delta(outsidePoint.x, point1.x)*lineSlope + point1.y;
             }
-            else if ((outsidePointRegion & RIGHT) != INSIDE) {
-                outsidePointX = xMax;
-                outsidePointY = delta(outsidePointX, point1X)*lineSlope + point1Y;
+            else if ((outsidePoint.region & RIGHT) != INSIDE) {
+                outsidePoint.x = xMax;
+                outsidePoint.y = delta(outsidePoint.x, point1.x)*lineSlope + point1.y;
             }
-            else if ((outsidePointRegion & BOTTOM) != INSIDE) {
-                outsidePointY = yMin;
-                outsidePointX = lineIsVertical
-                    ? point1X
-                    : delta(outsidePointY, point1Y)/lineSlope + point1X;
+            else if ((outsidePoint.region & BOTTOM) != INSIDE) {
+                outsidePoint.y = yMin;
+                outsidePoint.x = lineIsVertical
+                    ? point1.x
+                    : delta(outsidePoint.y, point1.y)/lineSlope + point1.x;
             }
-            else if ((outsidePointRegion & TOP) != INSIDE) {
-                outsidePointY = yMax;
-                outsidePointX = lineIsVertical
-                    ? point1X
-                    : delta(outsidePointY, point1Y)/lineSlope + point1X;
+            else if ((outsidePoint.region & TOP) != INSIDE) {
+                outsidePoint.y = yMax;
+                outsidePoint.x = lineIsVertical
+                    ? point1.x
+                    : delta(outsidePoint.y, point1.y)/lineSlope + point1.x;
             }
 
-            if (outsidePointRegion == point1Region) {
-                point1X = outsidePointX;
-                point1Y = outsidePointY;
-                point1Region  = regionCode(point1X, point1Y);
+            if (outsidePoint.isInTheSameRegionAs(point1)) {
+                point1.setPositionAndRegion(outsidePoint.x, outsidePoint.y);
             }
             else {
-                point2X = outsidePointX;
-                point2Y = outsidePointY;
-                point2Region = regionCode(point2X, point2Y);
+                point2.setPositionAndRegion(outsidePoint.x, outsidePoint.y);
             }
         }
-        line.setLine(point1X, point1Y, point2X, point2Y);
+        line.setLine(point1.x, point1.y, point2.x, point2.y);
         return true;
     }
 
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
     private static double delta(double value1, double value2) {
         return (Math.abs(value1 - value2) < MINIMUM_DELTA) ? 0 : (value1 - value2);
+    }
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+    class Point {
+        double x, y;
+        int region;
+
+        Point(double x, double y) {
+            setPositionAndRegion(x, y);
+        }
+
+        void setPositionAndRegion(double x, double y) {
+            this.x = x; this.y = y;
+            region = (x < xMin) ? LEFT : (x > xMax) ? RIGHT : INSIDE;
+            if (y < yMin)
+                region |= BOTTOM;
+            else if (y > yMax)
+                region |= TOP;
+        }
+
+        boolean isInTheSameRegionAs(Point otherPoint) {
+            return this.region == otherPoint.region;
+        }
     }
 
 }
